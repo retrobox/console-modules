1d4eb76bad4e5e705613388fce9fde2ec1e7c87f
Valentin Derouet <thevalentin61@gmail.com>
2018-05-26 00:31:42 +0200

    Rework everything

--- installretroboxtheme.sh
+++ installretroboxtheme.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "$(tput setaf 1)Installation du theme en cours... $(tput sgr0)"
-git clone --recursive --depth 1 "https://github.com/retrobox/overlay/themeRetroBox" "/etc/emulationstation/themes/retrobox_theme"
+git clone --recursive --depth 1 "https://github.com/retrobox/overlay/" "/home/pi/overlay/"
+cd /home/pi/overlay/themeRetroBox/
+sudo cp -r /home/pi/overlay/themeRetroBox /etc/emulationstation/themes/retrobox_theme
 echo "$(tput setaf 2)Fin de l'installation du theme..$(tput sgr0)."
