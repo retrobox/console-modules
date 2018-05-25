65f5582d593e7dbfe0fec4b1f4decc936a61e96a
Valentin Derouet <thevalentin61@gmail.com>
2018-05-26 00:00:03 +0200

    Add custom theme to RetroPi
    with splashscreen

---
+++ installretroboxtheme.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "$(tput setaf 1)Installation du theme en cours... $(tput sgr0)"
+git clone --recursive --depth 1 "https://github.com/retrobox/overlay/themeRetroBox" "/etc/emulationstation/themes/retrobox_theme"
+echo "$(tput setaf 2)Fin de l'installation du theme..$(tput sgr0)."
