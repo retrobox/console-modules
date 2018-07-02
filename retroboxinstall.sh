412fa47fa02459c67b441e666dd3c28f6fb6a7ca
Valentin Derouet <thevalentin61@gmail.com>
2018-07-02 23:03:27 +0200

    Update links

--- retroboxinstall.sh
+++ retroboxinstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "$(tput setaf 1)Installation de Websocket... $(tput sgr0)"
 sudo apt-get install unzip
-sudo wget http://retrobox.happyblocks.info/archives/setuptools-39.0.1.zip
+sudo wget http://static.retrobox.tech/package/setuptools-39.0.1.zip
 unzip setuptools-39.0.1.zip -d /home/pi/setuptools-39.0.1
-wget http://retrobox.happyblocks.info/archives/websocket_client-0.47.0.tar.gz
+sudo wget http://static.retrobox.tech/package/websocket_client-0.47.0.tar.gz
 tar -xvzf websocket_client-0.47.0.tar.gz
 sudo apt-get install python3-pip
 cd /home/pi/
