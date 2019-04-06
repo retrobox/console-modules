f838401ad2ffa02c38576d6fe74f907e4b26589f
Valentin Derouet <thevalentin61@gmail.com>
2019-04-06 21:14:30 +0200

    fix(issues) fix repo name and add rm command before git clone

--- installdisplayswitch.sh
+++ installdisplayswitch.sh
@@ -2,6 +2,7 @@
 echo "$(tput setaf 1)Installation de displaySwitch... $(tput sgr0)"
 cd /home/pi/
 sudo apt-get update
+sudo rm -r -f /home/pi/console-modules/
 sudo git clone --recursive --depth 1 "https://github.com/retrobox/console-modules/" "/home/pi/console-modules/"
 sudo \cp -r /home/pi/console-modules/displaySwitch/10-retropie.sh /etc/profile.d/
 sudo mkdir /home/pi/displayBoot
--- installretroboxtheme.sh
+++ installretroboxtheme.sh
@@ -2,6 +2,7 @@
 echo "$(tput setaf 1)Installation du theme en cours... $(tput sgr0)"
 cd /home/pi/
 sudo apt-get update
+sudo rm -r -f /home/pi/console-modules/
 sudo git clone --recursive --depth 1 "https://github.com/retrobox/console-modules/" "/home/pi/console-modules/"
 sudo \cp -r /home/pi/console-modules/themeRetroBox /etc/emulationstation/themes/retrobox_theme
 cd /home/pi/
