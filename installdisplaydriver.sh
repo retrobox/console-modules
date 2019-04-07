07e9461297edb2660e561b6374208404904f163a
Valentin Derouet <thevalentin61@gmail.com>
2019-04-07 10:38:32 +0200

    fix(repo name): issue w/ repo name and add rm before git clone

--- installdisplaydriver.sh
+++ installdisplaydriver.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-echo "$(tput setaf 1)Installation du driver ecran... $(tput sgr0)"
+echo "$(tput setaf 1)Installation du driver ecran 3.2inch... $(tput sgr0)"
 cd /home/pi/
 sudo apt-get update
+sudo rm -r -f /home/pi/waveshare-dtoverlays/
 git clone https://github.com/swkim01/waveshare-dtoverlays.git
-sudo cp waveshare-dtoverlays/waveshare32b-overlay.dtb /boot/overlays/waveshare32b.dtbo
-sudo wget https://raw.githubusercontent.com/retrobox/console-modules/master/displayDriver/config.txt
+sudo cp waveshare-dtoverlays/waveshare32b.dtbo /boot/overlays/waveshare32b.dtbo
+sudo -N wget https://raw.githubusercontent.com/retrobox/console-modules/master/displayDriver/config.txt
 sudo cp /home/pi/config.txt /boot/
 ls /dev/fb*
 sudo apt-get install cmake
+sudo rm -r -f /home/pi/rpi-fbcp/
 git clone https://github.com/tasanakorn/rpi-fbcp
 cd rpi-fbcp/
 mkdir build
@@ -15,7 +17,7 @@ cd build/
 cmake ..
 make
 sudo install fbcp /usr/local/bin/fbcp
-sudo wget https://raw.githubusercontent.com/retrobox/console-modules/master/displayDriver/rc.local
+sudo -N wget https://raw.githubusercontent.com/retrobox/console-modules/master/displayDriver/rc.local
 sudo cp /home/pi/rc.local /etc/
 cd /home/pi/
 echo "$(tput setaf 2)Fin de l'installation... Redémarrage$(tput sgr0)."
