79906c8cbf0f510484a41e7e6ca0410f94f4e27a
Le_Futuriste <spamfree@matthieubessat.fr>
2019-04-11 14:53:06 +0200

    feat: add unzip util installation

--- installoverlay.sh
+++ installoverlay.sh
@@ -2,20 +2,24 @@
 
 # require node to be installed
 # require zip and json gnome utils
+echo "installing unzip util..."
+
+sudo apt install -y unzip
+
 echo "installing overlay..."
 
 cd ~
 rm -R ~/overlay
 mkdir ~/overlay
-URL="https://github.com/retrobox/overlay/archive/"$(curl -s 'https://api.github.com/repos/retrobox/overlay/releases' | jq -r '.[0].tag_name')".zip"
+TAGNAME=$(curl -s 'https://api.github.com/repos/retrobox/overlay/releases' | jq -r '.[0].tag_name')
+echo "Using " $TAGNAME
+URL="https://github.com/retrobox/overlay/archive/" $TAGNAME ".zip"
 echo $URL
 wget -N -O overlay.zip $URL
 unzip -d ~/overlay ~/overlay.zip
-
-result=$(ls --format=commas ~/overlay)
-IFS=', ' read -r -a array <<< "$result"
-mv ~/overlay/$array/* ~/overlay
-rm -R ~/overlay/$array
+PATHOVERLAY=$(find ~/overlay -mindepth 1 -maxdepth 1 -type d)
+echo $PATHOVERLAY
+mv $PATHOVERLAY ~/overlay
 cd ~/overlay
 npm i
 
