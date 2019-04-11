9f0f0ac7d5740b07db51556e77bcdc1d01f11990
Le_Futuriste <spamfree@matthieubessat.fr>
2019-04-11 13:49:44 +0200

    fix: working node install script

--- installnode.sh
+++ installnode.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
 echo "installing node.js..."
-curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
-sudo apt install -y nodejs
-echo $(node -v)
-echo $(npm -v)
-echo "node.js installed"
+rm -R ~/nodeinstall
+mkdir ~/nodeinstall
+cd ~/nodeinstall
+wget -O node.tar.xz https://nodejs.org/dist/v10.15.3/node-v10.15.3-linux-armv6l.tar.xz
+echo "Downloaded."
+tar xf node.tar.xz
+echo "Extracted"
+rm node.tar.xz
+PATHNODE=$(find ~/nodeinstall -mindepth 1 -maxdepth 1 -type d)
+echo $PATHNODE
+sudo mv $PATHNODE ~/nodeinstall/node
+sudo cp -R  ~/nodeinstall/node/* /usr/local/
+echo "Node installed!"
+echo "Npm version:" $(npm -v)
+echo "Node version:" $(node -v)
