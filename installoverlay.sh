67f069f6c64d7b5f562f6f09ef0c99a65a63bffb
Le_Futuriste <spamfree@matthieubessat.fr>
2019-04-17 12:05:09 +0000

    update installoverlay.sh with correct colors and instructions

--- installoverlay.sh
+++ installoverlay.sh
@@ -2,29 +2,70 @@
 
 # require node to be installed
 # require zip and json gnome utils
-echo "installing unzip util..."
+echo " "
+echo "Installing unzip util..."
+echo " "
 
 sudo apt install -y unzip
 
-echo "installing json (jq) util..."
+echo ""
+echo "Installing json (jq) util..."
+echo ""
 
 sudo apt install -y jq
 
-echo "installing overlay..."
+echo ""
+echo "Installing overlay..."
+echo ""
 
 cd ~
 rm -R ~/overlay
-mkdir ~/overlay
+rm -R ~/overlayinstall
+mkdir ~/overlayinstall
+
+echo ""
+echo "Fetching release from github..."
+echo ""
+
 TAGNAME=$(curl -s 'https://api.github.com/repos/retrobox/overlay/releases' | jq -r '.[0].tag_name')
+
+echo ""
 echo "Using " $TAGNAME
-URL="https://github.com/retrobox/overlay/archive/" $TAGNAME ".zip"
+echo $TAGNAME
+PTAGNAME=$(echo $TAGNAME | tr "v" "-")
+echo $PTAGNAME
+URL="https://github.com/retrobox/overlay/archive/$TAGNAME.zip"
 echo $URL
-wget -N -O overlay.zip $URL
-unzip -d ~/overlay ~/overlay.zip
-PATHOVERLAY=$(find ~/overlay -mindepth 1 -maxdepth 1 -type d)
-echo $PATHOVERLAY
-mv $PATHOVERLAY ~/overlay
+echo ""
+
+echo "Downloading files..."
+echo ""
+wget -N -O ~/overlayinstall/overlay.zip $URL
+
+cd ~/overlayinstall
+
+echo ""
+echo "Extracting files..."
+echo ""
+unzip -d ~/overlayinstall ~/overlayinstall/overlay.zip
+
+mv ~/overlayinstall/overlay$PTAGNAME ~/overlay
+
 cd ~/overlay
+
+rm -Rf ~/overlayinstall
+echo ""
+echo "Installing npm dependencies..."
+echo ""
+tput sgr0
+
 npm i
 
-echo "overlay installed"
+tput bold
+tput setaf 2
+
+echo ""
+echo "Overlay installed"
+
+tput sgr0
+echo ""
